8f30e847dfd9470a92db5161a954b8268cd52f92
Chuck Lu <chuck.lu@qq.com>
2020-06-16 18:21:40 +0800

    add a .net core project

---
+++ SonarqueReport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SonarqueReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
